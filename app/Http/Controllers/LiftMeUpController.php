a00e04f73c934d55a17db90bf463ac9b17c4b1f8
Vishan Jayasinghearachchi <vishandanura@hotmail.com>
2016-05-02 12:30:12 +0530

    LiftMeUp API working

---
+++ app/Http/Controllers/LiftMeUpController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\liftMeUp;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class LiftMeUpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($type)
+    {
+        // Picks a random entry matching given selection criteria
+        $newPost = liftMeUp::where('type',$type)->orderByRaw("RAND()")->first();;
+
+        return response()->json([
+                'status' => 'success',
+                'data' => $newPost
+            ]
+        )->setStatusCode(200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/liftMeUp.php
+++ app/liftMeUp.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class liftMeUp extends Model
 {
-    //
+    protected $table = 'LiftMeUp';
 }
